0daf105d3dfa4f0de74d1f79fa7e63a81e937c34
Prasanna Vishnuram <brcprasanna@tcs.com>
2017-08-06 10:44:59 -0400

    Removed ignored files

--- app/build/generated/source/apt/main/debug/ram/king/com/divinebook/BR.java
+++
@@ -1,5 +0,0 @@
-package ram.king.com.divinebook;
-
-public class BR {
-        public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/main/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="8ea8167f-b814-4a6e-802a-8e338c40cd1c")
-public class DataBindingInfo {}
