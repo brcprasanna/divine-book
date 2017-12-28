65266848270ad74635a1cdf9fad05c70e0bbe42d
Prasanna Vishnuram <brcprasanna@tcs.com>
2017-12-28 17:10:50 -0500

    Push

---
+++ app/src/main/java/ram/king/com/divinebook/util/MyFirebaseMessagingService.java
@@ -0,0 +1,15 @@
+package ram.king.com.divinebook.util;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by i005 on 28/12/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
